4eddb910272b908d71a5f3cfc83a631c3c8f5239
Aruna2094 <arunaramalingam94@gmail.com>
2024-04-30 18:10:21 +0530

    message

--- src/org/multi/Rbibank.java
+++ src/org/multi/Rbibank.java
@@ -4,11 +4,12 @@ public class Rbibank extends Axisbank {
 	
 	private void savings() {
 		System.out.println("savings 3%");
+		System.out.println("done");
    }
 
 	private void current() {
 		System.out.println("current 4%");
-
+     System.out.println("done");
 	}
 	
 	
